8d2884542bb6423db0842e71b39c701e31013c65
jonassbarai <jonas_sbarai@hotmail.com>
2023-04-30 22:04:14 -0300

    Criação das classes Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+    Curso curso = new Curso("Lógica de Programação","Curso de lógica", 30);
+
+        Mentoria mentoria = new Mentoria("mentoria em java","descrição mentoria em java", LocalDate.now());
+
+        System.out.println(mentoria);
+
     }
 }
